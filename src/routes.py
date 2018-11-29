b6767d0d138d90f66ebb21bda12a1772a9f2b9ad
lucyxubroad <llx2@cornell.edu>
2018-11-28 22:50:54 -0500

    added authenntication stuff

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,8 @@
 import json
-from db import db, Post, Comment
+from db import db, Post, Comment, User
 from flask import Flask, request
+
+import users_dao
 
 db_filename = "confessions.db"
 app = Flask(__name__)
@@ -14,6 +16,19 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
     
+
+def extract_token(request):
+  auth_header = request.headers.get('Authorization')
+  if auth_header is None:
+      return False, json.dumps({'error': 'Missing authorization header.'})
+
+  # Header looks like "Authorization: Bearer <session token>"
+  bearer_token = auth_header.replace('Bearer ', '').strip()
+  if bearer_token is None or not bearer_token:
+      return False, json.dumps({'error': 'Invalid authorization header.'})
+
+  return True, bearer_token
+
 @app.route('/')
 def hello():
   return 'Hello World!'
@@ -138,6 +153,77 @@ def vote_comment(comment_id):
     db.session.commit()
     return json.dumps({'success': True, 'data': comment.serialize()}), 200
   return json.dumps({'success': False, 'error': 'Invalid comment!'}), 404
+
+@app.route('/register/', methods=['POST'])
+def register_account():
+    post_body = json.loads(request.data)
+    email = post_body.get('email')
+    password = post_body.get('password')
+
+    if email is None or password is None:
+        return json.dumps({'error': 'Invalid email or password'})
+
+    created, user = users_dao.create_user(email, password)
+
+    if not created:
+        return json.dumps({'error': 'User already exists.'})
+
+    return json.dumps({
+        'session_token': user.session_token,
+        'session_expiration': str(user.session_expiration),
+        'update_token': user.update_token
+    })
+
+@app.route('/login/', methods=['POST'])
+def login():
+    post_body = json.loads(request.data)
+    email = post_body.get('email')
+    password = post_body.get('password')
+
+    if email is None or password is None:
+        return json.dumps({'error': 'Invalid email or password'})
+
+    success, user = users_dao.verify_credentials(email, password)
+
+    if not success:
+        return json.dumps({'error': 'Incorrect email or password.'})
+
+    return json.dumps({
+        'session_token': user.session_token,
+        'session_expiration': str(user.session_expiration),
+        'update_token': user.update_token
+    })
+
+@app.route('/session/', methods=['POST'])
+def update_session():
+    success, update_token = extract_token(request)
+
+    if not success:
+        return update_token
+
+    try:
+        user = users_dao.renew_session(update_token)
+    except: 
+        return json.dumps({'error': 'Invalid update token.'})
+
+    return json.dumps({
+        'session_token': user.session_token,
+        'session_expiration': str(user.session_expiration),
+        'update_token': user.update_token
+    })
+
+@app.route('/secret/', methods=['GET'])
+def secret_message():
+    success, session_token = extract_token(request)
+
+    if not success:
+        return session_token 
+
+    user = users_dao.get_user_by_session_token(session_token)
+    if not user or not user.verify_session_token(session_token):
+        return json.dumps({'error': 'Invalid session token.'})
+
+    return json.dumps({'message': 'You have successfully implemented sessions.'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
